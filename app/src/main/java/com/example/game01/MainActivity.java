dd50db61928cbf502f42510afac74010c8afbccc
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-11 08:42:31 -0300

    Escolhendo opção pedra, papel ou tesoura

--- app/src/main/java/com/example/game01/MainActivity.java
+++ app/src/main/java/com/example/game01/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.game01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+    public void opcaoSelecionada(String msg){
+
+        TextView text = findViewById(R.id.textView3);
+        text.setText(msg);
     }
 }
