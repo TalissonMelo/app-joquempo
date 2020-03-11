8f4c294db348f24e7940147fe459b65337b0f27b
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-11 09:04:24 -0300

    Gerando escolha aleatoria do App

--- app/src/main/java/com/example/game01/MainActivity.java
+++ app/src/main/java/com/example/game01/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,9 +28,32 @@ public class MainActivity extends AppCompatActivity {
     public void selecionadoTesoura(View view){
         this.opcaoSelecionada("tesoura");
     }
+
     public void opcaoSelecionada(String msg){
+
+        ImageView img = findViewById(R.id.imageView5);
+
+        int numero = new Random().nextInt(3);
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+
+        String opcapApp = opcoes[numero];
+
+        switch (opcapApp){
+            case "pedra" :
+                img.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                img.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                img.setImageResource(R.drawable.tesoura);
+                break;
+            default:
+        }
 
         TextView text = findViewById(R.id.textView3);
         text.setText(msg);
     }
+
+
 }
